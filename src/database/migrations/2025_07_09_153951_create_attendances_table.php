06dc009b0df89393b8342342350bc1b4ef8b0aec
kihara ren <changshihaikeboluan@gmail.com>
2025-07-10 21:12:39 +0900

    マイグレーション

---
+++ src/database/migrations/2025_07_09_153951_create_attendances_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('user_id')->constrained();
+            $table->integer('year');
+            $table->date('date');
+            $table->time('syukkin')->nullable();
+            $table->time('taikin')->nullable();
+            $table->time('rests1')->nullable();
+            $table->time('reste1')->nullable();
+            $table->time('rests2')->nullable();
+            $table->time('reste2')->nullable();
+            $table->boolean('state')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
